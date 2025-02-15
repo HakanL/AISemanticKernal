a7f8deda0b67b8af42bd59b8362b8ba4375e86f6
Jeffrey Palermo <jeffrey@clear-measure.com>
2025-02-14 22:32:04 -0600

    Initial commit. for OllamaApiClient

---
+++ AISemanticKernal/OllamaApiClientTester.cs
@@ -0,0 +1,33 @@
+﻿using OllamaSharp;
+using OllamaSharp.Models.Chat;
+using System.Threading.Tasks;
+using Microsoft.Extensions.AI;
+using ChatRole = OllamaSharp.Models.Chat.ChatRole;
+
+namespace AISemanticKernal
+{
+    public class OllamaApiClientTester
+    {
+        [Test]
+        public void ShouldCallLocalLLM()
+        {
+            var ollamaApiClient = new OllamaApiClient("http://localhost:11434");
+            var chatRequest = new ChatRequest
+            {
+                Model = "llama3.2",
+                Messages =
+                [
+                    new Message(){Content = "What are your capabilities? Only list 3? use json response only", Role = ChatRole.User}
+                ]
+            };
+
+
+            var chatResponse = ollamaApiClient.ChatAsync(chatRequest);
+
+            foreach (var message in chatResponse.ToBlockingEnumerable())
+            {
+                Console.Write(message.Message.Content);
+            }
+        }
+    }
+}
